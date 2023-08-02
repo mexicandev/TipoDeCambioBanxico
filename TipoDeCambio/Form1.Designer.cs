286a6faf46872605566cda3b608e6e17dd937153
sistemas <lguerreroc@faro.com.mx>
2023-08-02 14:20:02 -0600

    Etiqueta al boton

--- TipoDeCambio/Form1.Designer.cs
+++ TipoDeCambio/Form1.Designer.cs
@@ -43,7 +43,7 @@ namespace TipoDeCambio
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
